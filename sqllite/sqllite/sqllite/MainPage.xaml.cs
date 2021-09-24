b98296c797dddedb20f24141a9b02ee8b0cff1fd
Martin Jørgensen <satoz.game@gmail.com>
2021-09-24 06:31:10 +0200

    push

---
+++ sqllite/sqllite/sqllite/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using sqllite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalDatabase
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text) && !string.IsNullOrWhiteSpace(roomEntry.Text))
+            {
+                await App.Database.SavePersonAsync(new Person
+                {
+                    Name = nameEntry.Text,
+                    Room = int.Parse(roomEntry.Text)
+                });
+
+                nameEntry.Text = roomEntry.Text = string.Empty;
+                collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+            }
+        }
+    }
+}
---
+++ sqllite/sqllite/sqllite/Person.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace LocalDatabase
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; } // ID PÅ LEJER
+        public string Name { get; set; } // NAVN 
+        public int Room { get; set; } // Værelse nr
+        public string OrderFood { get; set; } // NAVN 
+
+    }
+}
